a4a6f1cbdf6e6a0b58172e395a9b4627bf207f91
Liam Morrow <liam@citrusad.com>
2019-08-25 17:28:53 +1000

    Documentation

--- src/OrgnalR.SignalR/GrainFactoryProvider.cs
+++ src/OrgnalR.SignalR/GrainFactoryProvider.cs
@@ -3,6 +3,10 @@ using Orleans;
 
 namespace OrgnalR.SignalR
 {
+    /// <summary>
+    /// Provides the grain factory to the OrgnalR backplane.
+    /// See <see cref="GrainFactoryProvider"/> for a simple implementation.
+    /// </summary>
     public interface IGrainFactoryProvider
     {
         IGrainFactory GetGrainFactory();
