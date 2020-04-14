215f919361aad353eef36a08994dd43a15ed0320
Liam Morrow <liam@citrusad.com>
2020-04-14 20:09:57 +1000

    Added support for generic host

--- src/OrgnalR.OrleansSilo/Extensions.cs
+++ src/OrgnalR.OrleansSilo/Extensions.cs
@@ -47,6 +47,35 @@ namespace OrgnalR.Silo
         }
 
         /// <summary>
+        /// Adds the OrgnalR grains to the builder, and also automatically registers memory grain storage for group and user lists.
+        /// This is useful for local development, however it is recommended that you add a persistent storage for:
+        /// <see cref="GROUP_STORAGE_PROVIDER"/>, and <see cref="USER_STORAGE_PROVIDER"/>, and <see cref="MESSAGE_STORAGE_PROVIDER"/>
+        /// Then you may use <see cref="AddOrgnalR<T>(T builder)"/> to add orgnalr using the storage providers of your choice
+        /// </summary>
+        /// <param name="builder">The builder to configure</param>
+        /// <returns>The silo builder, configured with memory storage and grains for the OrgnalR backplane</returns>
+        public static ISiloBuilder AddOrgnalRWithMemoryGrainStorage(this ISiloBuilder builder, Action<OrgnalRSiloConfig>? configure = null)
+        {
+            try
+            {
+                builder.AddMemoryGrainStorage(Constants.GROUP_STORAGE_PROVIDER);
+            }
+            catch { /* Do nothing, already added  */}
+            try
+            {
+                builder.AddMemoryGrainStorage(Constants.USER_STORAGE_PROVIDER);
+            }
+            catch { /* Do nothing, already added  */}
+            try
+            {
+                builder.AddMemoryGrainStorage(Constants.MESSAGE_STORAGE_PROVIDER);
+            }
+            catch { /* Do nothing, already added  */}
+
+            return builder.AddOrgnalR(configure);
+        }
+
+        /// <summary>
         /// Adds the OrgnalR grains to the builder. This method is recommended for production use.
         /// You must configure storage providers for:
         /// <see cref="GROUP_STORAGE_PROVIDER"/>, and <see cref="USER_STORAGE_PROVIDER"/>, and <see cref="MESSAGE_STORAGE_PROVIDER"/>
@@ -55,6 +84,26 @@ namespace OrgnalR.Silo
         /// <param name="builder">The builder to configure</param>
         /// <returns>The silo builder, configured with grains for the OrgnalR backplane</returns>
         public static ISiloHostBuilder AddOrgnalR(this ISiloHostBuilder builder, Action<OrgnalRSiloConfig>? configure = null)
+        {
+            builder.ConfigureServices((_, services) =>
+            {
+                var conf = new OrgnalRSiloConfig();
+                configure?.Invoke(conf);
+                services.Add(new ServiceDescriptor(typeof(OrgnalRSiloConfig), conf));
+            });
+            builder.ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(AnonymousMessageGrain).Assembly).WithReferences());
+            return builder;
+        }
+
+        /// <summary>
+        /// Adds the OrgnalR grains to the builder. This method is recommended for production use.
+        /// You must configure storage providers for:
+        /// <see cref="GROUP_STORAGE_PROVIDER"/>, and <see cref="USER_STORAGE_PROVIDER"/>, and <see cref="MESSAGE_STORAGE_PROVIDER"/>
+        /// Alternatively, for local development, use: <see cref="AddOrgnalRWithMemoryGrainStorage<T>(T builder)"/>
+        /// </summary>
+        /// <param name="builder">The builder to configure</param>
+        /// <returns>The silo builder, configured with grains for the OrgnalR backplane</returns>
+        public static ISiloBuilder AddOrgnalR(this ISiloBuilder builder, Action<OrgnalRSiloConfig>? configure = null)
         {
             builder.ConfigureServices((_, services) =>
             {
