d9f2b2aaf02f0d424b2cc664a94311bb786c1e58
Brandon Behrens <brandonbehrens@gmail.com>
2022-11-29 09:20:47 -0600

    #1`9
    Got all the tests working except the first one.  Not sure how that one worked to begin with.

--- src/OrgnalR.Core/Provider/MethodMessage.cs
+++ src/OrgnalR.Core/Provider/MethodMessage.cs
@@ -5,8 +5,10 @@ namespace OrgnalR.Core.Provider
     [GenerateSerializer]
     public class MethodMessage
     {
+        [Id(0)]
         public string MethodName { get; }
 
+        [Id(1)]
         public object?[] Args { get; }
 
         public MethodMessage(string methodName, object?[] args)
