519fea9a1eeede90295014ca9cb78157f04e2dd7
Koen <koen@linker.io>
2023-10-26 01:50:22 +0100

    Ability to control / disable persistence

--- src/OrgnalR.Core/OrgnalRSiloConfig.cs
+++ src/OrgnalR.Core/OrgnalRSiloConfig.cs
@@ -4,6 +4,8 @@ namespace OrgnalR.Core
     public class OrgnalRSiloConfig
     {
         private int maxMessageRewind = 10;
+        private TimeSpan persistenceInterval = TimeSpan.FromSeconds(30);
+        private bool persistenceEnabled = true;
 
         public int MaxMessageRewind
         {
@@ -15,6 +17,23 @@ namespace OrgnalR.Core
                 }
                 maxMessageRewind = value;
             }
+        }
+
+        public TimeSpan PerstenceInterval
+        {
+            get => persistenceInterval; set
+            {
+                if (value < TimeSpan.Zero)
+                {
+                    throw new ArgumentOutOfRangeException($"{nameof(PerstenceInterval)} most be TimeSpan.Zero or larger! Provided [{value}]");
+                }
+                persistenceInterval = value;
+            }
+        }
+
+        public bool PersistenceEnabled
+        {
+            get => persistenceEnabled; set => persistenceEnabled = value;
         }
     }
 }
